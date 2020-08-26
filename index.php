c3fec10723a4cf1ba04713a7c1e89d0989fa9c1e
Rick Yentzer <ryentzer@gmail.com>
2020-08-26 17:58:11 -0400

    add loop

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
-<?php get_header(); ?>
-<?php require_once "components/navigation.php"; ?>
+<?php
+
+get_header();
+require_once "components/navigation.php";
+?>
+
 <div class="container">
-    <h2>Loop goes here</h2>
+	<?php
+	if ( have_posts() ):
+		while ( have_posts() ) : the_post(); ?>
+            <div class="post">
+                <h1 class="post-title"><a href="<?php the_permalink();?>"> <?php the_title(); ?></a></h1>
+                <?php the_excerpt(); ?>
+                <div class="post-meta">
+                    <span class="post-author"><?php echo get_the_author(); ?></span> |
+                    <span class="post-category"> <?php the_category(', '); ?></span>
+                </div>
+            </div>
+		<?php
+		endwhile; ?>
+	<?php
+	endif; ?>
 </div>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+
+<?php
+get_sidebar();
+get_footer();
+?>
